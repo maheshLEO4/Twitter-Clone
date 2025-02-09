198901b59f2039cd15da680915b762c5b1da6efc
maheshLEO4 <maheshmahi5345@gmail.com>
2025-02-09 12:17:24 +0530

    6-notification-route and controller added  and backend completed

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -27,6 +29,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8000, () => {
     console.log(`Server is running on port ${PORT}`);
